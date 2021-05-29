041230e5fb0dbec0e7df092fce4aaab73b4a5ab6
nrazam95 <nrazam95@gmail.com>
2021-05-29 21:35:58 +0800

    .

--- W4D7/minesweeper/minesweeper.rb
+++ W4D7/minesweeper/minesweeper.rb
@@ -5,7 +5,7 @@ class MinesweeperGame
   LAYOUTS = {
     small: { grid_size: 9, num_bombs: 10 },
     medium: { grid_size: 16, num_bombs: 40 },
-    large: { grid_size: 32, num_bombs: 160 } # whoa.
+    large: { grid_size: 32, num_bombs: 160 }
   }.freeze
 
   def initialize(size)
