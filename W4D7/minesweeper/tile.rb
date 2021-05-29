59f80d80a147140e35d91297fad2eb8453963a11
nrazam95 <nrazam95@gmail.com>
2021-05-29 20:51:38 +0800

    adding neighbours method and adjacent bomb count

--- W4D7/minesweeper/tile.rb
+++ W4D7/minesweeper/tile.rb
@@ -28,5 +28,22 @@ class Tile
         @flagged
     end
     
+    def neighbours
+        adjacent_coords = DELTAS.map do |dx, dy|
+            [pos[0] + dx, pos[1] + dy]
+        end.select do |row, col|
+            [row, col].all? do |coord|
+                coord.between?(0, @board.grid_size - 1)
+            end
+        end
+
+        adjacent_coords.map { |pos| @board[pos]}
+    end
+    
+    def adjacent_bomb_count
+        neighbours.select(&:bombed?).count
+    end
+
+    
     
 end
