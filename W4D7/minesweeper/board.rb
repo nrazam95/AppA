e39df00e8650c30425555f7566916f8182669445
nrazam95 <nrazam95@gmail.com>
2021-05-29 21:23:07 +0800

    adding attr reader

--- W4D7/minesweeper/board.rb
+++ W4D7/minesweeper/board.rb
@@ -2,6 +2,8 @@ require_relative 'tile'
 
 class Board
     
+    attr_reader :grid_size, :num_bombs
+
     def initialize(grid_size, num_bombs)
         @grid_size, @num_bombs = grid_size, num_bombs
 
