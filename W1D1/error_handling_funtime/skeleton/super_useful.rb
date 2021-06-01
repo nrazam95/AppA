7ccb7a9f6483ea7a4f87c4fd356fac032abce949
nrazam95 <nrazam95@gmail.com>
2021-06-01 13:16:11 +0800

    editting

--- W1D1/error_handling_funtime/skeleton/super_useful.rb
+++ W1D1/error_handling_funtime/skeleton/super_useful.rb
@@ -1,30 +1,49 @@
 # PHASE 2
-def convert_to_int(str)
-  Integer(str)
+FRUITS = ["apple", "banana", "orange"]
+
+class CoffeeError < StandardError
+  def message
+    "I can't have more caffeine. You can try again, later"
+  end
 end
 
-# PHASE 3
-FRUITS = ["apple", "banana", "orange"]
+class NotAFruitError < StandardError
+  def message
+    "That doesn't look like fruits. *run away* I'm running away from you"
+  end
+end
 
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
+  elsif maybe_fruit == "coffee"
+    raise CoffeeError
   else 
-    raise StandardError 
+    raise NotAFruitError 
   end 
 end
 
 def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
 
-  puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
+  begin
+    puts "Feed me a fruit! (Enter the name of a fruit:)"
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit) 
+  rescue CoffeeError => e
+    puts e.message
+  rescue NotAfruitError => e
+    puts e.message
+  end
 end  
 
-# PHASE 4
+# PHASE 3
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
+    raise ArgumentError.new("'name' cannot be blank") if name.empty?
+    raise ArgumentError.new("'yrs_known' must be greater or equal to 5") if yrs_known.to_i < 5
+    raise ArgumentError.new("'fav_pastime' cannot be blank") if fav_pastime.empty?
+
     @name = name
     @yrs_known = yrs_known
     @fav_pastime = fav_pastime
