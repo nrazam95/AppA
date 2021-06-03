85932e8fc9397752d93b3f83040abcb40308a975
nrazam95 <nrazam95@gmail.com>
2021-06-03 23:39:39 +0800

    moving pieces

--- W1D5/board.rb
+++ W1D5/board.rb
@@ -4,7 +4,7 @@ class Board
     attr_reader :rows
 
     def initialize(n = 8)
-        @rows = Array.new(n) { Array.new(n) { nil }}
+        @rows = Array.new(n) { Array.new(n, nil) }
     end
 
     def [](pos)
@@ -25,6 +25,7 @@ class Board
     def move_piece(start_pos, end_pos)
         piece = self[start_pos]
         self[end_pos] = piece
+        self[start_pos] = nil
     end
     
     
