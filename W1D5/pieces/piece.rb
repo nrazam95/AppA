c4d9b5090086da80f7b238c145d6a3888cceb7c1
nrazam95 <nrazam95@gmail.com>
2021-06-04 13:04:16 +0800

    editting piece

--- W1D5/pieces/piece.rb
+++ W1D5/pieces/piece.rb
@@ -1,35 +1,39 @@
 class Piece
     attr_reader :board, :color
     attr_accessor :pos
-
-    def intialize(color, board, pos)
-        @color, @board, @pos = color, board, pos
-        board.add_piece(self, pos)        
+  
+    def initialize(color, board, pos)
+      raise 'invalid color' unless %i(white black).include?(color)
+      raise 'invalid pos' unless board.valid_pos?(pos)
+  
+      @color, @board, @pos = color, board, pos
+  
+      board.add_piece(self, pos)
     end
-    
+  
     def to_s
-        " #{symbol} "
+      " #{symbol} "
     end
-    
+  
     def empty?
-        false
+      false
     end
-    
+  
     def symbol
-        raise NotImplementedError
+      # subclass implements this with unicode chess char
+      raise NotImplementedError
     end
-    
+  
     def valid_moves
-        moves.reject { |end_pos| move_into_check?(end_pos)}
+      moves.reject { |end_pos| move_into_check?(end_pos) }
     end
-
+  
     private
-
+  
     def move_into_check?(end_pos)
-        test_board = board.dup
-        test_board.move_piece!(pos, end_pos)
-        test_board.in_check?(color)
+      test_board = board.dup
+      test_board.move_piece!(pos, end_pos)
+      test_board.in_check?(color)
     end
-    
-    
-end
+  end
+  
