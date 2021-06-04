d3fab84de7a4476dde9ed911dc0338d9c61b86c0
nrazam95 <nrazam95@gmail.com>
2021-06-04 11:28:51 +0800

    adding ! to the method

--- W1D5/pieces/piece.rb
+++ W1D5/pieces/piece.rb
@@ -27,7 +27,7 @@ class Piece
 
     def move_into_check?(end_pos)
         test_board = board.dup
-        test_board.move_piece(pos, end_pos)
+        test_board.move_piece!(pos, end_pos)
         test_board.in_check?(color)
     end
     
