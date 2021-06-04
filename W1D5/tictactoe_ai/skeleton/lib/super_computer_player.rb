c12d7e558d2de354321dfe0b2400c3b5182ab554
nrazam95 <nrazam95@gmail.com>
2021-06-05 03:36:11 +0800

    adding tic tac toe AI

--- W1D5/tictactoe_ai/skeleton/lib/super_computer_player.rb
+++ W1D5/tictactoe_ai/skeleton/lib/super_computer_player.rb
@@ -2,12 +2,23 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+
+    possible_moves = node.children.shuffle
+
+    node = possible_moves.find { |child| child.winning_node?(mark) }
+
+    return node.prev_move_pos if node
+    node = possible_moves.find { |child| !child.losing_node?(mark) }
+
+    return node.prev_move_pos if node
+    raise "Wait, it looks like I'm going to lose?"
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Play the brilliant computer!"
-  hp = HumanPlayer.new("Jeff")
+  hp = HumanPlayer.new("Lex")
   cp = SuperComputerPlayer.new
 
   TicTacToe.new(hp, cp).run
